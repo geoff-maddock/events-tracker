d9e89167f45ad356cdb1d64c9f48d5fc72ed97fe
Geoff Maddock <geoff.maddock@gmail.com>
2017-06-26 21:45:01 +0000

    Added image linking to the event edit page.

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -8,6 +8,8 @@
 	@include('events.crumbs', ['slug' => $event->slug ? $event->slug : $event->id])
 </h1>
 
+<div class="row">
+  <div class="col-md-8">
 	{!! Form::model($event, ['route' => ['events.update', $event->id], 'method' => 'PATCH']) !!}
 
 		@include('events.form', ['action' => 'update'])
@@ -17,9 +19,66 @@
 	<P>{!! delete_form(['events.destroy', $event->id]) !!}</P>
 
 	<P><a href="{!! URL::route('events.index') !!}" class="btn btn-info">Return to list</a></P>
+  </div>
+
+  <div class="col-md-4">
+    @if ($user && (Auth::user()->id == $event->user->id || $user->id == Config::get('app.superuser') ) )  
+    <form action="/events/{{ $event->id }}/photos" class="dropzone" id="myDropzone" method="POST">
+      <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    </form>
+    @endif
+
+    <br style="clear: left;"/>
+
+    @foreach ($event->photos->chunk(4) as $set)
+    <div class="row">
+    @foreach ($set as $photo)
+      <div class="col-md-2">
+      <a href="/{{ $photo->path }}" data-lightbox="{{ $photo->path }}"><img src="/{{ $photo->thumbnail }}" alt="{{ $event->name}}"  style="max-width: 100%;"></a>
+      @if ($user && (Auth::user()->id == $event->user->id || $user->id == Config::get('app.superuser') ) )  
+      @if ($signedIn || $user->id == Config::get('app.superuser')) 
+        {!! link_form_icon('glyphicon-trash text-warning', $photo, 'DELETE', 'Delete the photo') !!}
+        @if ($photo->is_primary)
+        {!! link_form_icon('glyphicon-star text-primary', '/photos/'.$photo->id.'/unsetPrimary', 'POST', 'Primary Photo [Click to unset]') !!}
+        @else
+        {!! link_form_icon('glyphicon-star-empty text-info', '/photos/'.$photo->id.'/setPrimary', 'POST', 'Set as primary photo') !!}
+        @endif
+      @endif
+      @endif
+      </div>
+    @endforeach
+    </div>
+    @endforeach
+    </div>
+
+  </div>
+
+</div>
 @stop
 
 @section('scripts.footer')
+<script src="//cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/dropzone.js"></script>
+<script>
+Dropzone.autoDiscover = false;
+$(document).ready(function(){
+
+  var myDropzone = new Dropzone('#myDropzone');
+  myDropzone.options.addPhotosForm = {
+    maxFilesize: 3,
+    accept: ['.jpg','.png','.gif'],
+    init: function () {
+              myDropzone.on("complete", function (file) {
+                  location.href = 'events/{{ $event->id }}'
+                  location.reload();
+
+              });
+          }
+  };
+
+  myDropzone.options.addPhotosForm.init();
+  
+})
+</script>
 <script type="text/javascript">
 $('input.delete').on('click', function(e){
   e.preventDefault();
--- resources/views/events/form.blade.php
+++ resources/views/events/form.blade.php
@@ -1,7 +1,7 @@
 
 <div class="row">
  
-	<div class="form-group col-md-8 {{$errors->has('name') ? 'has-error' : '' }}">
+	<div class="form-group col-md-12 {{$errors->has('name') ? 'has-error' : '' }}">
 	{!! Form::label('name','Name') !!}
 	{!! Form::text('name', null, ['class' =>'form-control']) !!}
 	{!! $errors->first('name','<span class="help-block">:message</span>') !!}
@@ -11,7 +11,7 @@
 
 <div class="row">
  
-	<div class="form-group col-md-8 {{$errors->has('slug') ? 'has-error' : '' }}">
+	<div class="form-group col-md-12 {{$errors->has('slug') ? 'has-error' : '' }}">
 	{!! Form::label('slug','Slug') !!}
 	{!! Form::text('slug', null, ['placeholder' => 'Unique name for this event (will validate)', 'class' =>'form-control']) !!}
 	{!! $errors->first('slug','<span class="help-block">:message</span>') !!}
@@ -20,7 +20,7 @@
 
 <div class="row">
  
-	<div class="form-group col-md-8 {{$errors->has('slug') ? 'has-error' : '' }}">
+	<div class="form-group col-md-12 {{$errors->has('slug') ? 'has-error' : '' }}">
 	{!! Form::label('short','Short Description') !!}
 	{!! Form::text('short', null, ['class' =>'form-control']) !!}
 	{!! $errors->first('short','<span class="help-block">:message</span>') !!}
@@ -30,7 +30,7 @@
 
 <div class="row">
  
-	<div class="form-group col-md-8">
+	<div class="form-group col-md-12">
 	{!! Form::label('description','Description') !!}
 	{!! Form::textarea('description', null, ['class' =>'form-control']) !!}
 	{!! $errors->first('description','<span class="help-block">:message</span>') !!}
@@ -40,19 +40,19 @@
 
 <div class="row">
 
-	<div class="form-group col-md-2 {{$errors->has('event_type_id') ? 'has-error' : '' }}">
+	<div class="form-group col-md-3 {{$errors->has('event_type_id') ? 'has-error' : '' }}">
 	{!! Form::label('event_type_id','Event type:') !!}
 	{!! Form::select('event_type_id', $eventTypes, (isset($event->event_type_id) ? $event->event_type_id : NULL), ['class' =>'form-control']) !!}
 	{!! $errors->first('event_type_id','<span class="help-block">:message</span>') !!}
 	</div>
 
-	<div class="form-group col-md-2">
+	<div class="form-group col-md-3">
 	{!! Form::label('venue_id','Venue') !!}
 	{!! Form::select('venue_id', $venues, (isset($event->venue_id) ? $event->venue_id : NULL), ['class' =>'form-control']) !!}
 	{!! $errors->first('venue_id','<span class="help-block">:message</span>') !!}
 	</div>
 
-	<div class="form-group col-md-2">
+	<div class="form-group col-md-3">
 	{!! Form::label('promoter_id','Promoter') !!}
 	{!! Form::select('promoter_id', $promoters, (isset($event->promoter_id) ? $event->promoter_id : NULL), ['class' =>'form-control']) !!}
 	{!! $errors->first('promoter_id','<span class="help-block">:message</span>') !!}
@@ -60,13 +60,13 @@
 </div>
 
 <div class="row">
-	<div class="form-group col-md-2">
+	<div class="form-group col-md-3">
 	{!! Form::label('soundcheck_at','Soundcheck At:') !!}
 	{!! Form::dateTimeLocal('soundcheck_at', (isset($event->soundcheck_at)) ? $event->soundcheck_at->format('Y-m-d\\TH:i') : NULL, ['class' =>'form-control']) !!}
 	{!! $errors->first('soundcheck_at','<span class="help-block">:message</span>') !!}
 	</div>
 
-	<div class="form-group col-md-2"> 
+	<div class="form-group col-md-3"> 
 	{!! Form::label('door_at','Door At:') !!}
 	{!! Form::dateTimeLocal('door_at', (isset($action) && isset($event->door_at)) ? $event->door_at->format('Y-m-d\\TH:i') : NULL, ['class' =>'form-control']) !!}
 	{!! $errors->first('door_at','<span class="help-block">:message</span>') !!}
@@ -75,13 +75,13 @@
 </div>
 
 <div class="row">
-	<div class="form-group col-md-2 {{$errors->has('start_at') ? 'has-error' : '' }}">
+	<div class="form-group col-md-3 {{$errors->has('start_at') ? 'has-error' : '' }}">
 	{!! Form::label('start_at','Start At:') !!}
 	{!! Form::dateTimeLocal('start_at', (isset($action) && isset($event->start_at)) ?  $event->start_at->format('Y-m-d\\TH:i') : Carbon\Carbon::now()->format('Y-m-d\\TH:i'), ['class' =>'form-control']) !!}
 	{!! $errors->first('start_at','<span class="help-block">:message</span>') !!}
 	</div>
 
-	<div class="form-group col-md-2">
+	<div class="form-group col-md-3">
 	{!! Form::label('end_at','End At:') !!}
 	{!! Form::dateTimeLocal('end_at', (isset($action) && isset($event->end_at)) ? $event->end_at->format('Y-m-d\\TH:i') : NULL, ['class' =>'form-control']) !!}
 	{!! $errors->first('end_at','<span class="help-block">:message</span>') !!}
@@ -91,19 +91,19 @@
 <div class="row">
  
 
-	<div class="form-group col-md-2">
+	<div class="form-group col-md-3">
 	{!! Form::label('presale_price','Presale Price:') !!}
 	{!! Form::text('presale_price', null, ['class' =>'form-control']) !!}
 	{!! $errors->first('presale_price','<span class="help-block">:message</span>') !!}
 	</div>
 
-	<div class="form-group col-md-2">
+	<div class="form-group col-md-3">
 	{!! Form::label('door_price','Door Price:') !!}
 	{!! Form::text('door_price', null, ['class' =>'form-control']) !!}
 	{!! $errors->first('door_price','<span class="help-block">:message</span>') !!}
 	</div>
 
-	<div class="form-group col-md-2">
+	<div class="form-group col-md-3">
 	{!! Form::label('min_age','Min Age:') !!}
 	{!! Form::select('min_age', [ '0' => 'All Ages', '18' => '18', '21' => '21'], (isset($event->min_age) ? $event->min_age : NULL), ['class' =>'form-control']) !!}
 	{!! $errors->first('min_age','<span class="help-block">:message</span>') !!}
@@ -111,14 +111,14 @@
 </div>
 
 <div class="row">
-	<div class="form-group col-md-4">
+	<div class="form-group col-md-6">
 	{!! Form::label('primary_link','Primary Link:') !!}
 	{!! Form::text('primary_link', null, ['class' =>'form-control']) !!}
 	{!! $errors->first('primary_link','<span class="help-block">:message</span>') !!}
 	<button type="button" class="btn btn-info" id='import-link'>Import</button>
 	</div>
 
-	<div class="form-group col-md-4 {{$errors->has('ticket_link') ? 'has-error' : '' }}">
+	<div class="form-group col-md-6 {{$errors->has('ticket_link') ? 'has-error' : '' }}">
 	{!! Form::label('ticket_link','Ticket Link:') !!}
 	{!! Form::text('ticket_link', null, ['class' =>'form-control']) !!}
 	{!! $errors->first('ticket_link','<span class="help-block">:message</span>') !!}
@@ -126,7 +126,7 @@
 </div>
 
 <div class="row">
-	<div class="form-group col-md-2 {{$errors->has('visibility_id') ? 'has-error' : '' }}">
+	<div class="form-group col-md-3 {{$errors->has('visibility_id') ? 'has-error' : '' }}">
 	{!! Form::label('visibility_id','Visibility:') !!}
 	{!! Form::select('visibility_id', $visibilities, (isset($event->visibility_id) ? $event->visibility_id : NULL), ['class' =>'form-control']) !!}
 	{!! $errors->first('visibility_id','<span class="help-block">:message</span>') !!}
@@ -134,7 +134,7 @@
 </div>
 
 <div class="row">
-	<div class="form-group col-md-2">
+	<div class="form-group col-md-3">
 	{!! Form::label('series_id','Series:') !!}
 	{!! Form::select('series_id', $seriesList, (isset($event->series_id) ? $event->series_id : NULL), ['class' =>'form-control']) !!}
 	{!! $errors->first('series_id','<span class="help-block">:message</span>') !!}
@@ -143,7 +143,7 @@
 
 
 <div class="row">
-	<div class="form-group col-md-2">
+	<div class="form-group col-md-6">
 	{!! Form::label('entity_list','Related Entities:') !!}
 	{!! Form::select('entity_list[]', $entities, null, ['id' => 'entity_list', 'class' =>'form-control', 'multiple']) !!}
 	{!! $errors->first('entities','<span class="help-block">:message</span>') !!}
@@ -152,7 +152,7 @@
 
 
 <div class="row">
-	<div class="form-group col-md-2">
+	<div class="form-group col-md-6">
 	{!! Form::label('tag_list','Tags:') !!}
 	{!! Form::select('tag_list[]', $tags, null, ['id' => 'tag_list', 'class' =>'form-control', 'multiple']) !!}
 	{!! $errors->first('tags','<span class="help-block">:message</span>') !!}
@@ -161,7 +161,7 @@
 
 
 <div class="row">
-	<div class="form-group col-md-2">
+	<div class="form-group col-md-3">
 	{!! Form::label('attending','Attending') !!}
 	@if (isset($event))
 	  {{ $event->attending }}
