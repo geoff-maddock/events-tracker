cb321eb44073a25016e812ab8da93338d015d21a
geoff.maddock <geoff.maddock@gmail.com>
2017-07-05 10:23:54 -0700

    Fixed light-theme, removed medium-theme for now.

--- config/constants.php
+++ config/constants.php
@@ -2,6 +2,6 @@
 return [
     'days' => ['' => '', 'Sunday' => 'Sunday', 'Monday' => 'Monday', 'Tuesday' => 'Tuesday', 'Wednesday' => 'Wednesday', 'Thursday' => 'Thursday', 'Friday' => 'Friday', 'Saturday' => 'Saturday'],
     'weeks' => ['' => '', 'First' => 'First', 'Second' => 'Second' , 'Third' => 'Third', 'Fourth' => 'Fourth', 'Last' => 'Last'],
-    'themes' => ['' => '', 'dark-theme' => 'Dark Theme', 'medium-theme' =>  'Medium Theme',  'light-theme' => 'Light Theme']
+    'themes' => ['' => '', 'dark-theme' => 'Dark Theme', 'light-theme' => 'Light Theme']
 ];
 ?>
