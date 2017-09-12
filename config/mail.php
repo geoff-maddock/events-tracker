f5c243129c95706344273631cfe3d7076092cbaf
geoff.maddock <geoff.maddock@gmail.com>
2017-09-11 23:49:10 -0700

    Upgraded to laravel v5.4

--- config/mail.php
+++ config/mail.php
@@ -121,4 +121,14 @@ return [
 
 	'pretend' => false,
 
+    /*
+     * Markdown Config
+     */
+    'markdown' => [
+        'theme' => 'default',
+
+        'paths' => [
+            resource_path('views/vendor/mail'),
+        ],
+    ],
 ];
