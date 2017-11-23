6fa622a8c9f1760301bd06d0d06392135234d209
geoff.maddock <geoff.maddock@gmail.com>
2017-11-22 18:00:39 -0800

    Added links to grid cell image title to click to event and venue.

--- resources/views/events/cell.blade.php
+++ resources/views/events/cell.blade.php
@@ -1,7 +1,7 @@
 @if ($primary = $event->getPrimaryPhoto())
     <div style="padding: 5px;">
-        <a href="/{{ $primary->path }}" data-lightbox="grid" data-title="{!! $event->start_at->format('l F jS Y') !!} {{ $event->name }} @ {{ $event->venue->name or '' }}" data-lightbox="{{ $primary->path }}" title="{!! $event->start_at->format('l F jS Y') !!} {{ $event->name }} @ {{ $event->venue->name or '' }}" data-toggle="tooltip" data-placement="bottom">
-            <img src="/{{ $primary->thumbnail }}" alt="{{ $event->name}}"  >
+        <a href="/{{ $primary->path }}" data-lightbox="grid" data-title="{!! $event->start_at->format('l F jS Y') !!} <a href='/events/{{ $event->id }}'>{{ $event->name }}</a> @ <a href='/entities/{{ $event->venue->id or '' }}'>{{ $event->venue->name or '' }}</a>" data-lightbox="{{ $primary->path }}" title="{!! $event->start_at->format('l F jS Y') !!} {{ $event->name }} @ {{ $event->venue->name or '' }}" data-toggle="tooltip" data-placement="bottom">
+            <img src="/{{ $primary->thumbnail }}" alt="{{ $event->name }}"  >
         </a>
     </div>
 @endif
