5bfd69c8f7e0c7a15ad8574b876596ecbb98332f
geoff.maddock <geoff.maddock@gmail.com>
2017-12-03 23:38:45 -0800

    Issue #81.  Updated series related threads and links to related entities and tags.

--- resources/views/series/show.blade.php
+++ resources/views/series/show.blade.php
@@ -33,9 +33,6 @@
 			Next is {{ $series->nextEvent() ? $series->nextEvent()->start_at->format('l F jS Y') : $series->cycleFromFoundedAt()->format('l F jS Y') }} (not yet created)<br>
 		@endif
 
-
-
-
 	@endif
 	</p>
 
@@ -61,7 +58,9 @@
 	@unless ($series->entities->isEmpty())
 	Related Entities:
 		@foreach ($series->entities as $entity)
-		<span class="label label-tag"><a href="/series/relatedto/{{ $entity->slug }}">{{ $entity->name }}</a></span>
+		<span class="label label-tag"><a href="/series/relatedto/{{ $entity->slug }}">{{ $entity->name }}</a>
+		<a href="{!! route('entities.show', ['id' => $entity->id]) !!}" title="Show this entity."><span class='glyphicon glyphicon-link text-info'></span></a>
+		</span>
 		@endforeach
 	@endunless
 	</P>
@@ -69,7 +68,8 @@
 	@unless ($series->tags->isEmpty())
 	<P>Tags:
 	@foreach ($series->tags as $tag)
-		<span class="label label-tag"><a href="/series/tag/{{ $tag->name }}">{{ $tag->name }}</a></span>
+		<span class="label label-tag"><a href="/series/tag/{{ $tag->name }}">{{ $tag->name }}</a>
+		<a href="{!! route('tags.show', ['slug' => $tag->name]) !!}" title="Show this tag."><span class='glyphicon glyphicon-link text-info'></span></a></span>
 		@endforeach
 	@endunless
 	</P>
@@ -95,51 +95,85 @@
 			</div>
 		</div>
 
-
-
-
 	</div>
 </div>
 
-<div class="col-md-6">
-@if ($user && (Auth::user()->id == $series->user->id || $user->id == Config::get('app.superuser') ) )
-<form action="/series/{{ $series->id }}/photos" class="dropzone" id="myDropzone" method="POST">
-	<input type="hidden" name="_token" value="{{ csrf_token() }}">
-</form>
-@endif
+	<div class="col-md-6">
+	@if ($user && (Auth::user()->id == $series->user->id || $user->id == Config::get('app.superuser') ) )
+	<form action="/series/{{ $series->id }}/photos" class="dropzone" id="myDropzone" method="POST">
+		<input type="hidden" name="_token" value="{{ csrf_token() }}">
+	</form>
+	@endif
 
-<br style="clear: left;"/>
+	<br style="clear: left;"/>
 
-@foreach ($series->photos->chunk(4) as $set)
-<div class="row">
-@foreach ($set as $photo)
-	<div class="col-md-2">
-        <a href="/{{ $photo->path }}" data-lightbox="{{ $photo->path }}" title="Click to see enlarged image" data-toggle="tooltip" data-placement="bottom"><img src="/{{ $photo->thumbnail }}" alt="{{$photo->name}}"  style="max-width: 100%;" ></a>
-	@if ($user && (Auth::user()->id == $series->user->id || $user->id == Config::get('app.superuser') ))	
-				{!! link_form_icon('glyphicon-trash text-warning', $photo, 'DELETE', 'Delete the photo') !!}
-				@if ($photo->is_primary)
-				{!! link_form_icon('glyphicon-star text-primary', '/photos/'.$photo->id.'/unsetPrimary', 'POST', 'Primary Photo [Click to unset]') !!}
-				@else
-				{!! link_form_icon('glyphicon-star-empty text-info', '/photos/'.$photo->id.'/setPrimary', 'POST', 'Set as primary photo') !!}
+	@foreach ($series->photos->chunk(4) as $set)
+		<div class="row">
+		@foreach ($set as $photo)
+			<div class="col-md-2" style="padding-bottom: 10px;">
+				<a href="/{{ $photo->path }}" data-lightbox="{{ $photo->path }}" title="Click to see enlarged image" data-toggle="tooltip" data-placement="bottom"><img src="/{{ $photo->thumbnail }}" alt="{{$photo->name}}"  style="max-width: 100%;" ></a>
+				@if ($user && (Auth::user()->id == $series->user->id || $user->id == Config::get('app.superuser') ))
+							{!! link_form_icon('glyphicon-trash text-warning', $photo, 'DELETE', 'Delete the photo') !!}
+							@if ($photo->is_primary)
+							{!! link_form_icon('glyphicon-star text-primary', '/photos/'.$photo->id.'/unsetPrimary', 'POST', 'Primary Photo [Click to unset]') !!}
+							@else
+							{!! link_form_icon('glyphicon-star-empty text-info', '/photos/'.$photo->id.'/setPrimary', 'POST', 'Set as primary photo') !!}
+							@endif
 				@endif
+			</div>
+		@endforeach
+		</div>
+	@endforeach
+</div>
+	<br>
+	<div class="row">
+	<!-- RELATED THREADS -->
+	@if ($threads)
+		@php
+			$thread = $threads->first()
+		@endphp
+			@if (isset($thread) && count($thread) > 0)
+			<div class="col-md-6">
+				<div class="panel panel-info">
+
+					<div class="panel-heading">
+						<h3 class="panel-title">Posts
+							<a href="#" ><span class='label label-tag pull-right' data-toggle="tooltip" data-placement="bottom"  title="# of Threads that match this search term.">{{ count($thread)}}</span></a>
+						</h3>
+					</div>
+
+					<div class="panel-body">
+						<table class="table forum table-striped">
+							@include('threads.briefFirst', ['thread' => $thread])
+							@include('posts.briefList', ['thread' => $thread, 'posts' => $thread->posts])
+						</table>
+
+						<div class="col-lg-12">
+
+							@if ($thread->is_locked)
+								<P class="text-center">This thread has been locked.</P>
+							@else
+								@if ($signedIn)
+									Add new post as <strong>{{ $user->name }}</strong>
+									<form method="POST" action="{{ $thread->path().'/posts' }}">
+										{{ csrf_field() }}
+										<div class="form-group">
+											<textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+										</div>
+										<button type="submit" class="btn btn-default">Post</button>
+									</form>
+
+								@else
+									<p class="text-center">Please <a href="{{ url('/login')}}">sign in</a> to participate in this discussion.</p>
+								@endif
+							@endif
+						</div>
+					</div>
+				</div>
+			</div>
+		@endif
 	@endif
 	</div>
-@endforeach
-</div>
-@endforeach
-
-	<div class="row">
-		<div class="col-sm-12">
-			<div class="bs-component">
-							@include('threads.list', ['threads' => $threads])
-							{!! $threads->render() !!}
-			</div>
-		</div>
-	</div>
-
-</div>
-
-
 </div>
 @stop
 
