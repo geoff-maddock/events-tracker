cb4177783ed21a4811508b796d16cd36fe7365a8
geoff.maddock <geoff.maddock@gmail.com>
2017-12-12 20:17:34 -0800

    Issue #179.  Added FB import code to create occurrence.

--- resources/views/series/createOccurrence.blade.php
+++ resources/views/series/createOccurrence.blade.php
@@ -3,7 +3,7 @@
 @section('title','Event Series -  Create Occurrence')
 
 @section('content')
-
+	<script src="{{ asset('/js/facebook-sdk.js') }}"></script>
 	<h1>{{ $series->name}} </h1> 
 	<h2>Add Occurrence: {{ $event->name }}</h2>
 
@@ -14,4 +14,9 @@
 	{!! Form::close() !!}
 
 	<P><a href="{!! URL::route('series.index') !!}" class="btn btn-info">Return to list</a></P>
+
+@stop
+
+@section('scripts.footer')
+	<script src="{{ asset('/js/facebook-event.js') }}"></script>
 @stop
