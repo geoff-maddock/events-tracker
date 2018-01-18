98a4f230c3ed16c3247b385551ad923b4bd2e71f
Geoff Maddock <geoff.maddock@gmail.com>
2018-01-18 09:13:30 +0000

    Fixed password reset issue.

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,9 @@ $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('re
 $this->post('register', 'Auth\RegisterController@register');
 
 // Password Reset Routes...
-$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.forgot');
 $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 $this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('/', 'PagesController@home')->name('home');
