564f27eb709c181d1c547d0d26f1a254122568c1
Geoff Maddock <geoff.maddock@gmail.com>
2018-04-08 00:04:29 +0000

    Fixed issue with responsive more menu.

---
+++ app/Events/EventCreated.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use App\Event;
+
+class EventCreatedBanned
+{
+    use InteractsWithSockets, SerializesModels;
+
+    public  $event;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Event $event)
+    {
+        $this->event = $event;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
