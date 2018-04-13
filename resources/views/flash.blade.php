364e72f1c1dac805c4d459f7a9e2c247a1d59634
Geoff Maddock <geoff.maddock@gmail.com>
2018-04-13 05:41:06 +0000

    Attemped to fix SWAL issue.

--- resources/views/flash.blade.php
+++ resources/views/flash.blade.php
@@ -1,12 +1,14 @@
 @if (session()->has('flash_message'))
 
 	<script>
-	swal({ 
-	  title: "{{ session('flash_message.title') }}",   
-	  text: "{!! session('flash_message.message') !!}",   
-	  type: "{{ session('flash_message.level') }}",   
+	swal({
+	  title: "{{ session('flash_message.title') }}",
+	  text: "{!! session('flash_message.message') !!}",
+	  type: "{{ session('flash_message.level') }}",
 	  timer: 1700,
-	  showConfirmation: false,
+		showConfirmButton: true,
+		closeModal: true,
+		closeOnCancel: true,
 	});
 	</script>
 
