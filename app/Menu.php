3beb233ec0bce69139458753434618dc9ab4ce86
Geoff Maddock <geoff.maddock@gmail.com>
2019-09-19 06:40:01 +0000

    Issue #283.  Added migrations.

---
+++ app/Menu.php
@@ -0,0 +1,60 @@
+<?php namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Menu extends Eloquent {
+
+
+    /**
+     * The storage format of the model's date columns.
+     *
+     * @var string
+     */
+    //protected $dateFormat = 'Y-m-d\\TH:i';
+
+	/**
+	 * @var Array
+	 *
+	 **/
+	protected $fillable = [
+	'name', 'slug', 'menu_parent_id', 'visibility_id'
+	];
+
+ 
+	protected $dates = ['created_at','updated_at'];
+
+	
+	/**
+	 * A menu has at most one menu parent
+	 *
+	 */
+	public function menuParent()
+	{
+		return $this->hasOne('App\Menu','id','menu_parent_id');
+	}
+
+	/**
+	 * A location has one visibility
+	 *
+	 */
+	public function visibility()
+	{
+		return $this->hasOne('App\Visibility','id','visibility_id');
+	}
+
+
+    /**
+     * Returns visible events
+     *
+     */
+    public function scopeVisible($query, $user)
+    {
+
+        $public = Visibility::where('name','=','Public')->first();
+        $guarded = Visibility::where('name','=','Guarded')->first();
+
+        $query->where('visibility_id','=', $public ? $public->id : NULL )->orWhere('created_by','=',($user ? $user->id : NULL));
+
+    }
+}
