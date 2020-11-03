4e763d945315868d989786f914110c79ea97cac2
Geoff Maddock <geoff.maddock@gmail.com>
2020-11-03 19:58:33 +0000

    Fixed incorrect parameter in the tag show route.

--- resources/views/tags/edit.blade.php
+++ resources/views/tags/edit.blade.php
@@ -6,7 +6,7 @@
 
 
 	<h2>Tag . EDIT : {{ $tag->name }}</h2>
-	<br> 	<a href="{!! route('tags.show', ['id' => $tag->id]) !!}" class="btn btn-primary">Show Tag</a> <a href="{!! URL::route('tags.index') !!}" class="btn btn-info">Return to list</a>
+	<br> 	<a href="{!! route('tags.show', ['tag' => $tag->id]) !!}" class="btn btn-primary">Show Tag</a> <a href="{!! URL::route('tags.index') !!}" class="btn btn-info">Return to list</a>
 
 
 	{!! Form::model($tag, ['route' => ['tags.update', $tag->id], 'method' => 'PATCH']) !!}
