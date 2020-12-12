49449057db7d24b8ca5942a1babbeed81af8c047
Geoff Maddock <geoff.maddock@gmail.com>
2020-12-12 00:01:45 +0000

    Issue#381.  Fixed ActivityTest functional test.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
