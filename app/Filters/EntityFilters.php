ea56d401e0fa13758f2ade30d0c5bdb3dbaa5015
Geoff Maddock <geoff.maddock@gmail.com>
2021-02-20 23:01:52 +0000

    Issue#330.  Converted entity index to use the list entity result builder. Still need to update other index methods.

--- app/Filters/EntityFilters.php
+++ app/Filters/EntityFilters.php
@@ -7,16 +7,41 @@ class EntityFilters extends QueryFilter
     public function name($value = null)
     {
         if (isset($value)) {
-            return $this->builder->where('name', $value);
+            return $this->builder->where('entities.name', 'like', '%' . $value . '%');
         } else {
             return $this->builder;
         }
     }
 
-    public function type($value = null)
+    public function entity_type($value = null)
     {
         if (isset($value)) {
-            return $this->builder->where('entity_type', $value);
+            return $this->builder->whereHas('entityType', function ($q) use ($value) {
+                $q->where('name', '=', ucfirst($value));
+            });
+            ;
+        } else {
+            return $this->builder;
+        }
+    }
+
+    public function tag($value = null)
+    {
+        if (isset($value)) {
+            return $this->builder->whereHas('tags', function ($q) use ($value) {
+                $q->where('name', '=', ucfirst($value));
+            });
+        } else {
+            return $this->builder;
+        }
+    }
+
+    public function role($value = null)
+    {
+        if (isset($value)) {
+            return $this->builder->whereHas('roles', function ($q) use ($value) {
+                $q->where('name', '=', ucfirst($value));
+            });
         } else {
             return $this->builder;
         }
