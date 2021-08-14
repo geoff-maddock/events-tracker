3048baf077aaaa4441b12b033ce63b629e877b87
Geoff Maddock <geoff.maddock@gmail.com>
2021-08-14 08:09:05 +0000

    Updated the events upcoming home page to work with bootstrap 5.

--- resources/views/events/show/actions.blade.php
+++ resources/views/events/show/actions.blade.php
@@ -1,12 +1,12 @@
-@if ($user && (Auth::user()->id == $event->user->id || $user->id == Config::get('app.superuser') ) )
+@if ($user && $event->user && (Auth::user()->id == $event->user->id || $user->id == Config::get('app.superuser') ) )
     <a href="{!! route('events.edit', ['event' => $event->id]) !!}" class="btn btn-primary">Edit Event</a>
 @endif
 
-@if (!$event->series_id && $user && (Auth::user()->id == $event->user->id || $user->id == Config::get('app.superuser') ) )
+@if (!$event->series_id && $event->user && $user && (Auth::user()->id == $event->user->id || $user->id == Config::get('app.superuser') ) )
     <a href="{!! route('events.createSeries', ['id' => $event->id]) !!}" title="Create an event series based on this event." class="btn btn-primary"><span class='glyphicon glyphicon-fire'></span> Create Series</a>
 @endif
 
-@if (!$thread && $user && (Auth::user()->id == $event->user->id || $user->id == Config::get('app.superuser')) )
+@if (!$thread && $user && $event->user && (Auth::user()->id == $event->user->id || $user->id == Config::get('app.superuser')) )
     <a href="{!! route('events.createThread', ['id' => $event->id]) !!}" title="Create an thread related to this event." class="btn btn-primary"><span class='glyphicon glyphicon-comment'></span> Create Thread</a>
 @endif
 
