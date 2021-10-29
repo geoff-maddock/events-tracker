261f5238ad964fe270315ef69eeadefc582b9c7d
Geoff Maddock <geoff.maddock@gmail.com>
2021-10-29 03:57:46 +0000

    Added google tags handling.

--- resources/views/partials/analytics.blade.php
+++ resources/views/partials/analytics.blade.php
@@ -7,3 +7,10 @@
 
   gtag('config', '{{ config('app.analytics') }}');
 </script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','{{ config('app.google_tags')}}');</script>
+  <!-- End Google Tag Manager -->
