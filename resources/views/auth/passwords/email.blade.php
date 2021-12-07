2894b09a25a156ab8cc56180ac0d01eb4e40710b
Geoff Maddock <geoff.maddock@gmail.com>
2021-12-07 08:13:27 +0000

    Issue #688.  Fixed UI to password reset forms.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+				<h1 class="display-6 text-primary">Reset Password</h1>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -23,7 +23,7 @@
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control form-background" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -34,7 +34,7 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-6 col-md-offset-4 my-2">
                                 <button type="submit" class="btn btn-primary">
                                     Send Password Reset Link
                                 </button>
