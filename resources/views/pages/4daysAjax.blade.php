7d3568d1a21499ba6068a7070309e530c8d517a1
Geoff Maddock <geoff.maddock@gmail.com>
2022-05-17 03:03:03 +0000

    Issue #844. Changed the home page so that it will use ajax to add the next events below.

--- resources/views/pages/4daysAjax.blade.php
+++ resources/views/pages/4daysAjax.blade.php
@@ -22,7 +22,7 @@
 	<!-- DISPLAY THE NEXT FOUR DAYS OF EVENTS -->
 	<?php $today = \Carbon\Carbon::now('America/New_York'); ?>
 	
-	<div class="row small-gutter" id="home">
+	<div class="row small-gutter home" id="home">
 		@for ($offset = 0; $offset < 4; $offset++)
 		<?php $day = \Carbon\Carbon::parse($date)->addDay($offset); ?>
 			<section class="day" data-num="{{ $offset }}" id="day-position-{{ $offset }}" href="/events/day/{{ $day->format('Y-m-d') }}">
@@ -31,7 +31,7 @@
 		@endfor
 	</div>
 
-	<div class="row d-block d-sm-none">
+	<div class="row d-block">
 		<div class="col-sm-12">
 			<ul class="list-style-none"  class="mt-0">
 				<li>{!! link_to_route('events.add', 'Next Events', ['date' => $next_day_window->format('Ymd')], ['id' => 'add-event', 'class' => 'next-events page-link text-nowrap']) !!}</li>
