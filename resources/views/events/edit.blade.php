604579b69f626f669c61e1792e6c25d83271427d
Geoff Maddock <geoff.maddock@gmail.com>
2022-12-20 07:02:09 +0000

    Extracted select2 includes.

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -2,6 +2,12 @@
 
 @section('title', 'Event Edit')
 
+@section('select2.include')
+<!-- Select2 -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.1.1/dist/select2-bootstrap-5-theme.min.css" />
+@endsection
+
 @section('content')
 
 <script src="{{ asset('/js/facebook-sdk.js') }}"></script>
--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -2,6 +2,12 @@
 
 @section('title', 'Thread Add')
 
+@section('select2.include')
+<!-- Select2 -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.1.1/dist/select2-bootstrap-5-theme.min.css" />
+@endsection
+
 @section('content')
 
 <h1 class="display-crumbs text-primary">Add a New Thread</h1>
