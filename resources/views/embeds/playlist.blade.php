66cefe9c4c2902f678490f70f9b70f3c39645650
Geoff Maddock <geoff.maddock@gmail.com>
2022-12-29 19:23:30 +0000

    Issue #983, #961.  Added loading spinner for audio ajax load, moved future events to top for entities.

--- resources/views/embeds/playlist.blade.php
+++ resources/views/embeds/playlist.blade.php
@@ -16,9 +16,23 @@
 </div>
 @else
     @if (isset($event))
-        <div id="playlist-{{ $event->id}}" class="playlist-id" data-url="/events/{{ $event->id }}/load-embeds"></div>
+        <div id="playlist-{{ $event->id}}" class="playlist-id" data-url="/events/{{ $event->id }}/load-embeds">
+            <div class="card-body">
+				<div class="load-spinner">
+					<div class="double-bounce1"></div>
+					<div class="double-bounce2"></div>
+				</div>
+			</div>
+        </div>
     @endif
     @if (isset($entity))
-    <div id="playlist-{{ $entity->id}}" class="playlist-id" data-url="/entities/{{ $entity->id }}/load-embeds"></div>
-@endif
+        <div id="playlist-{{ $entity->id}}" class="playlist-id" data-url="/entities/{{ $entity->id }}/load-embeds">
+            <div class="card-body">
+                <div class="load-spinner">
+                    <div class="double-bounce1"></div>
+                    <div class="double-bounce2"></div>
+                </div>
+            </div>
+        </div>
+    @endif
 @endif
--- resources/views/entities/show.blade.php
+++ resources/views/entities/show.blade.php
@@ -278,31 +278,33 @@
 		</div>
 		<div class="row event-container">
 
-				<div class="event-columns">
-					<div class="card bg-dark">
+			<div class="event-columns">
+				<div class="card bg-dark">
 
-							<h5 class="card-header bg-primary">Past Events <span class="badge rounded-pill bg-dark float-end"><a href="{{ url('events/related-to/'.$entity->slug) }}">{{ $entity->pastEvents()->total() }}</a></span></h5>
+					<h5 class="card-header bg-primary">Future Events <span class="badge rounded-pill bg-dark float-end"><a href="{{ url('events/related-to/'.$entity->slug) }}">{{ $entity->futureEvents()->total() }}</a></span></h5>
 
-							<div class="card-body">
-								@include('events.vertical-list', ['events' => $entity->pastEvents()])
-								{!! $entity->pastEvents()->render() !!}
-							</div>
-
-					</div>
-				</div>
-
-				<div class="event-columns">
-					<div class="card bg-dark">
-
-						<h5 class="card-header bg-primary">Future Events <span class="badge rounded-pill bg-dark float-end"><a href="{{ url('events/related-to/'.$entity->slug) }}">{{ $entity->futureEvents()->total() }}</a></span></h5>
-
-							<div class="card-body">
-								@include('events.vertical-list', ['events' => $entity->futureEvents()])
-								{!! $entity->futureEvents()->render() !!}
-							</div>
+						<div class="card-body">
+							@include('events.vertical-list', ['events' => $entity->futureEvents()])
+							{!! $entity->futureEvents()->render() !!}
 						</div>
 					</div>
 				</div>
+			</div>
+
+			<div class="event-columns">
+				<div class="card bg-dark">
+
+						<h5 class="card-header bg-primary">Past Events <span class="badge rounded-pill bg-dark float-end"><a href="{{ url('events/related-to/'.$entity->slug) }}">{{ $entity->pastEvents()->total() }}</a></span></h5>
+
+						<div class="card-body">
+							@include('events.vertical-list', ['events' => $entity->pastEvents()])
+							{!! $entity->pastEvents()->render() !!}
+						</div>
+
+				</div>
+			</div>
+
+
 		</div>
 	</div>
 </div>
