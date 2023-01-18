7a92458c8332697b32dab82a7ede50c570f70106
Geoff Maddock <geoff.maddock@gmail.com>
2023-01-18 02:32:32 +0000

    Changed the code to pull the url for the origin from the window.

--- resources/assets/js/swagger.js
+++ resources/assets/js/swagger.js
@@ -1,6 +1,11 @@
 import SwaggerUI from 'swagger-ui'
 import 'swagger-ui/dist/swagger-ui.css';
+
+const {
+    host, hostname, href, origin, pathname, port, protocol, search
+} = window.location
+
 SwaggerUI({
     dom_id: '#swagger-api',
-    url: 'https://dev.arcane.city/postman/schemas/api.yml',
+    url: origin + '/postman/schemas/api.yml',
 });
