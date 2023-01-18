7a804056e29114263d0e39148dc50fd851b183cd
Geoff Maddock <geoff.maddock@gmail.com>
2023-01-18 06:47:52 +0000

    Added select to to thread list page.

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -4,6 +4,12 @@
 Forum Thread "{{ $thread->name }}"
 @endsection 
 
+@section('select2.include')
+<!-- Select2 -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.1.1/dist/select2-bootstrap-5-theme.min.css" />
+@endsection
+
 @section('content')
 
 	<h1 class="display-crumbs text-primary">Forum
