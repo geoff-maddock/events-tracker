f86802325f129b1e36fde0f2ccff744dc093d981
Geoff Maddock <geoff.maddock@gmail.com>
2023-02-07 06:46:58 +0000

    Fixes select2 on post edit.

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,9 +2,15 @@
 
 @section('title','Post Edit')
 
+@section('select2.include')
+<!-- Select2 -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.1.1/dist/select2-bootstrap-5-theme.min.css" />
+@endsection
+
 @section('content')
 
-<h1 class="display-6 text-primary">Post . Edit  @include('posts.crumbs', ['slug' => $post->slug ? $post->slug : $post->id])</h1>
+<h1 class="display-crumbs text-primary">Post . Edit  @include('posts.crumbs', ['slug' => $post->slug ? $post->slug : $post->id])</h1>
 
 	{!! Form::model($post, ['route' => ['posts.update', $post->id], 'method' => 'PATCH']) !!}
 
