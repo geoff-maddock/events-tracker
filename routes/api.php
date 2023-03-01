335984c180179aac7313d0f2feb27942c21b1549
Geoff Maddock <geoff.maddock@gmail.com>
2023-03-01 06:35:53 +0000

    Added shield API route example

--- routes/api.php
+++ routes/api.php
@@ -45,6 +45,10 @@ Route::middleware('auth.basic')->name('api.')->group(function () {
 
 });
 
+// routes protected by the shield middleware
+Route::middleware('shield')->name('shield.')->group(function () {
+});
+
 // calendar routes - these are used by the web app for dynamic loading
 Route::get('calendar-events', 'EventsController@calendarEventsApi')->name('calendarEvents.api');
 Route::get('tag-calendar-events', 'EventsController@tagCalendarEventsApi')->name('tagCalendarEvents.api');
