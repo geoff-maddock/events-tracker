6cd493d35fe82815f352a42fa6b058b9603ad069
Geoff Maddock <geoff.maddock@gmail.com>
2023-04-24 06:28:14 +0000

    Added select2 styles for series create occurrence.

--- resources/views/series/createOccurrence.blade.php
+++ resources/views/series/createOccurrence.blade.php
@@ -2,22 +2,21 @@
 
 @section('title','Event Series -  Create Occurrence')
 
-@section('content')
-    <script src="{{ asset('/js/facebook-sdk.js') }}"></script>
-    <script async defer src="https://connect.facebook.net/en_US/sdk.js"></script>
+@section('select2.include')
+<!-- Select2 -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.1.1/dist/select2-bootstrap-5-theme.min.css" />
+@endsection
 
+@section('content')
 	<h2>{{ $series->name}} </h2>
 	<h4>Add Occurrence: {{ $event->name }}</h4>
-
-	{!! Form::model($event, ['route' => ['events.store']]) !!}
+	
+	{!! Form::open(['route' => 'events.store', 'class' => 'form-container']) !!}
 
 		@include('events.form', ['action' => 'createOccurrence'])
 
 	{!! Form::close() !!}
 
 	<P><a href="{!! URL::route('series.index') !!}" class="btn btn-info">Return to list</a></P>
-@stop
-
-@section('scripts.footer')
-	<script src="{{ asset('/js/facebook-event.js') }}"></script>
 @stop
