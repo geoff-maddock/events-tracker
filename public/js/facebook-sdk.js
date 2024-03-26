87afb95b8cbf7f62bee88bfac0fcdbdb6d09c7b5
Geoff Maddock <geoff.maddock@gmail.com>
2024-03-26 03:44:51 +0000

    Updated facebook SDK to 19

--- public/js/facebook-sdk.js
+++ public/js/facebook-sdk.js
@@ -1,13 +1,13 @@
-window.fbAsyncInit = function() {
+window.fbAsyncInit = function () {
 
-    FB.init({
-      appId      : facebookAppId, // should be replaced with FB app id
-      autoLogAppEvents : true,
-      status     : true,
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v12.0'
-    });
-    FB.AppEvents.logPageView();
-    console.log('fbAsyncInit complete')
-  };
+  FB.init({
+    appId: facebookAppId, // should be replaced with FB app id
+    autoLogAppEvents: true,
+    status: true,
+    cookie: true,
+    xfbml: true,
+    version: 'v19.0'
+  });
+  FB.AppEvents.logPageView();
+  console.log('fbAsyncInit complete')
+};
