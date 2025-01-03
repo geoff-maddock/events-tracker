326d8511453f49e6698f46acfc0cf76e16084e3a
Geoff Maddock <geoff.maddock@gmail.com>
2025-01-03 06:47:59 +0000

    Added vercel to cors.

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'paths' => ['api/*'],
-    'allowed_origins' => ['https://dev.arcane.city','https://arcane.city','http://localhost:3000','http://localhost:5173'],
+    'allowed_origins' => ['https://dev.arcane.city','https://arcane.city','http://localhost:3000','http://localhost:5173','https://arcane-city-frontend.vercel.app'],
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     'allowed_headers' => ['Content-Type', 'Authorization'],
     'exposed_headers' => [],
