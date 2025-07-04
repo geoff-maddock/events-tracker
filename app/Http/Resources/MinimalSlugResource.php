79c1ada27e2c8aa4c99671989fba130ff85106f9
Geoff Maddock <geoff.maddock@gmail.com>
2025-07-03 20:05:21 -0400

    Added a minimal resource that includes slug (#1446)

---
+++ app/Http/Resources/MinimalSlugResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Generic resource for returning minimal slug relationship data.
+ */
+class MinimalSlugResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->resource->id,
+            'name' => $this->resource->name,
+            'slug' => $this->resource->slug
+        ];
+    }
+}
