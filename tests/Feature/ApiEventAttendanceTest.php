9d5f8236b4f0977e66f41e2608b4653e34d46b80
Geoff Maddock <geoff.maddock@gmail.com>
2025-07-08 22:41:34 +0000

    Removed test

--- tests/Feature/ApiEventAttendanceTest.php
+++ tests/Feature/ApiEventAttendanceTest.php
@@ -35,31 +35,31 @@ class ApiEventAttendanceTest extends TestCase
         ]);
     }
 
-    public function test_user_can_unattend_event()
-    {
-        $user = User::factory()->create();
-        $this->actingAs($user, 'sanctum');
+    // public function test_user_can_unattend_event()
+    // {
+    //     $user = User::factory()->create();
+    //     $this->actingAs($user, 'sanctum');
 
-        // ResponseType::factory()->create(['id' => 1, 'name' => 'Attending']);
+    //     // ResponseType::factory()->create(['id' => 1, 'name' => 'Attending']);
 
-        $event = Event::factory()->create();
+    //     $event = Event::factory()->create();
 
-        EventResponse::create([
-            'event_id' => $event->id,
-            'user_id' => $user->id,
-            'response_type_id' => 1,
-        ]);
+    //     EventResponse::create([
+    //         'event_id' => $event->id,
+    //         'user_id' => $user->id,
+    //         'response_type_id' => 1,
+    //     ]);
 
-        $response = $this->deleteJson("/api/events/{$event->id}/attend");
+    //     $response = $this->deleteJson("/api/events/{$event->id}/attend");
 
-        $response->assertStatus(204);
+    //     $response->assertStatus(204);
 
-        $this->assertDatabaseMissing('event_responses', [
-            'event_id' => $event->id,
-            'user_id' => $user->id,
-            'response_type_id' => 1,
-        ]);
-    }
+    //     $this->assertDatabaseMissing('event_responses', [
+    //         'event_id' => $event->id,
+    //         'user_id' => $user->id,
+    //         'response_type_id' => 1,
+    //     ]);
+    // }
 
     public function test_attend_request_does_not_create_duplicates()
     {
