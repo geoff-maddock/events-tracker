088f30675a9397efa8a227adfd30ae97b855b8f0
Geoff Maddock <geoff.maddock@gmail.com>
2025-07-30 22:13:47 +0000

    Added update to tagline

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -11,7 +11,7 @@
 		<h3 class="font-weight-bold">{{ config('app.tagline')}} <a href="#" class="toggler" id="event-close-box" data-bs-target="#jumboContainer" data-bs-toggle="collapse" aria-expanded="false" aria-controls="jumboContainer" role="button">...</a></h3>
 		<div id="jumboContainer" class="collapsible collapse show">
 
-			<p>Arcane City is a calendar and guide to events, weekly and  monthly series, promoters, artists, producers, djs, venues and other entities.</p>
+			<p>Arcane City is a calendar and guide to events, weekly and  monthly series, promoters, artists, producers, djs, venues and other entities that are part of the Pittsburgh scene.</p>
 			<P>
 			<a href="{!! URL::route('events.index') !!}" class="btn btn-info mt-2 mr-2">Show all events</a>
 			<a href="{!! URL::route('events.future') !!}" class="btn btn-info mt-2 mr-2">Show future events</a>
