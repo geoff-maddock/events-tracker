ad66da8cd15789dd4a13f3800ac0cb4169e4ea64
Geoff Maddock <geoff.maddock@gmail.com>
2025-09-02 05:05:18 +0000

    Changes the URL used in resetting the password based on the request.

--- app/Http/Controllers/Api/PasswordResetController.php
+++ app/Http/Controllers/Api/PasswordResetController.php
@@ -26,7 +26,10 @@ class PasswordResetController extends Controller
             return response()->json(['message' => 'Invalid secret'], 401);
         }
 
-        $status = Password::broker()->sendResetLink(['email' => $data['email']]);
+        // if the frontend-url is sent in the request, pass that in
+        $frontendUrl = $request->input('frontend-url');
+
+        $status = Password::broker()->sendResetLink(['email' => $data['email'], 'frontend-url' => $frontendUrl]);
 
         if ($status === Password::RESET_LINK_SENT) {
             $user = User::where('email', $data['email'])->first();
